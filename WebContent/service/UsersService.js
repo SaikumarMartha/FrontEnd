70e855064de4fb549b82b530cca67ee252298ff7
Saikumar Martha <Saikumar Martha@Martha>
2017-12-26 15:52:56 +0530

    Registration

---
+++ WebContent/service/UsersService.js
@@ -0,0 +1,31 @@
+app.factory('UserService',function($http)
+{
+	
+	var  BASSE_URL="http://localhost:8081/middleware";
+	
+    var userService={}
+    
+    userService.registerUser = function(user){
+    	
+    	console.log( "success")
+    	return $http.post(BASSE_URL +"/registration",user)
+    }
+    
+    userService.login=function(user){
+    	return $http.post(BASSE_URL +"/login",user)
+    }
+ 
+
+
+userService.logout=function(user){
+	return $http.get(BASSE_URL +"/logout",user)
+
+}
+/*
+userService.login=function(user){
+	return $http.post(BASSE_URL +"/home",user)
+}*/
+
+    
+return userService;
+})
